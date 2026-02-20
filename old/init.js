3e29c449f56399f6b5633ced7fae952a1c7e9e23
30ff55 <30ff55zzzz@gmail.com>
2026-02-20 15:07:56 -0600

    Update init.js

--- old/init.js
+++ old/init.js
@@ -11,14 +11,17 @@ const scene = createScene(engine, canvas);
 
 const xrHelper = await scene.createDefaultXRExperienceAsync();
 
-document.getElementById("enterVRBtn").addEventListener("click", () => {
+const button = document.getElementById("enterVRBtn")
+button.addEventListener("click", () => {
     xrHelper.baseExperience.enterXRAsync("immersive-vr", "local-floor")
 
     xrHelper.baseExperience.onStateChangedObservable.add((state) => {
         if (state === BABYLON.WebXRState.ENTERING_XR) {
             canvas.style.display = "block";
+            button.style.display = "none";
         } else if (state === BABYLON.WebXRState.EXITING_XR) {
             canvas.style.display = "none";
+            button.style.display = "block";
         }
     });
 });
