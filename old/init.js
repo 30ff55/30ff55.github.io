449e246cbb4d7e2faa9c62bc4dade5a35fe407fc
30ff55 <30ff55zzzz@gmail.com>
2026-02-20 14:35:54 -0600

    Update init.js

--- old/init.js
+++ old/init.js
@@ -3,10 +3,10 @@ import createScene from './app.js';
 const BABYLON = window.BABYLON;
 const canvas = document.getElementById('renderCanvas');
 
-engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+const engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
 window.engine = engine;
 window.canvas = canvas;
 
-const scene = createScene();
+const scene = createScene(engine, canvas);
 engine.runRenderLoop(()=>scene.render());
 addEventListener('resize', ()=>engine.resize());
